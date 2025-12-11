640e56a921edc2294b56cede8774f7299f0b7d98
Abhijit Kshirsagar <kabhijit@iitdh.ac.in>
2025-12-11 17:35:33 +0530

    Main Updates

--- compile.sh
+++ compile.sh
@@ -22,6 +22,7 @@
 # compile for online version
 pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinetrue \input{MAIN.tex}"
 pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinetrue \input{MAIN.tex}"
+
 mv MAIN.pdf booklet_online.pdf
 
 # compile for printed version
@@ -31,3 +32,5 @@ pdflatex -synctex=1 -interaction=nonstopmode "\newif\ifOnline \Onlinefalse \inpu
 #pdftk main.pdf cat 5-9 output timetable.pdf
 #pdftk main.pdf cat 32-36 output list_posters.pdf
 mv MAIN.pdf booklet_printed.pdf
+
+cp booklet_online.pdf MAIN.pdf
